350985cfd2e582b52d74128f27b4b2c2c96fa8df
tamarmar <99142279+tamarmar@users.noreply.github.com>
2025-02-23 16:56:15 +0200

    Update example.test.js

--- __tests__/example.test.js
+++ __tests__/example.test.js
@@ -5,6 +5,6 @@ test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
 
-test('adds 2 + 2 to equal 4', () => {
-  expect(sum(2, 2)).toBe(4); // This test will fail intentionally
+test('adds 2 + 2 to equal 5', () => {
+  expect(sum(2, 2)).toBe(5); // This test will fail intentionally
 });
